908311c4a7efebccddd47a836cb7d9b5c147a983
Dmitry trubnikov <d.trubnikov@me.com>
2024-09-25 13:49:03 +0300

    Initial Commit

---
+++ GitHubExercise/GitHubExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExerciseApp.swift
+//  GitHubExercise
+//
+//  Created by Dmitry Trubnikov on 25/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
